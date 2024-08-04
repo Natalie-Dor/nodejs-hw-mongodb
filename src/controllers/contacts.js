3184dd54e8d66f636fd4510d2d3b6e5286b12166
Natalie-Dor <varnata000@gmail.com>
2024-08-04 18:04:48 +0300

    pagination, filtration

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,11 +6,24 @@ import {
   getContactByIdService,
   updateContactByIdService,
 } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 // import { createContactSchema } from '../validation/contacts.js';
 
-export const getAllContactsController = async (req, res, next) => {
-  const contacts = await getAllContactsService();
+export const getAllContactsController = async (req, res) => {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
+
+  const contacts = await getAllContactsService({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
   res.json({
     status: 200,
     message: 'Successfully found contacts!',
