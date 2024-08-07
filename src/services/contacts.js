762666a34b65ee8f966288b717144ba283529128
Natalie-Dor <varnata000@gmail.com>
2024-08-07 18:57:34 +0300

    fixed pagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -11,7 +11,14 @@ export const getAllContactsService = async ({
   const skip = page > 0 ? (page - 1) * perPage : 0;
 
   const contactQuery = Contact.find();
-
+  const [countContacts, contacts] = await Promise.all([
+    Contact.find().merge(contactQuery).countDocuments(),
+    contactQuery
+      .sort({ [sortBy]: sortOrder })
+      .skip(skip)
+      .limit(limit)
+      .exec(),
+  ]);
   if (filter.isFavourite) {
     contactQuery.where('isFavourite').equals(filter.isFavourite);
   }
@@ -19,36 +26,16 @@ export const getAllContactsService = async ({
     contactQuery.where('contactType').equals(filter.contactType);
   }
 
-  const [contacts, count] = await Promise.all([
-    contactQuery
-      .sort({ [sortBy]: sortOrder })
-      .skip(skip)
-      .limit(limit)
-      .exec(),
-    Contact.countDocuments(contactQuery),
-  ]);
-
-  // const [contacts, count] = await Promise.all([
-  //   Contact.find()
-  //     .sort({ [sortBy]: sortOrder })
-  //     .skip(skip)
-  //     .limit(limit)
-  //     .exec(),
-  //   Contact.countDocuments(),
-  // ]);
-  // const contacts = await Contact.find().skip(skip).limit(limit).exec();
-  // const count = await Contact.countDocuments();
-  const totalPages = Math.ceil(count / perPage);
+  const totalPages = Math.ceil(countContacts / perPage);
   return {
     contacts,
     page,
     perPage,
-    totalItems: count,
+    totalItems: countContacts,
     totalPages,
     hasNextPage: totalPages - page > 0,
     hasPreviousPage: page > 1,
   };
-  return Contact.find().merge(contactQuery).exec();
 };
 
 export const getContactByIdService = (contactId) => Contact.findById(contactId);
